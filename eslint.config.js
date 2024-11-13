e6bcb5a09cc681d7f74577d646da28cf33d4b5f3
lhvision <lhvision@outlook.com>
2024-11-13 20:36:16 +0800

    chore: 配置 oxlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,9 @@
 import antfu from '@antfu/eslint-config'
+import oxlint from 'eslint-plugin-oxlint'
 
 export default antfu({
   formatters: true,
-})
+  rules: {
+    'jsdoc/check-param-names': 'off',
+  },
+}, oxlint.configs['flat/recommended'])
