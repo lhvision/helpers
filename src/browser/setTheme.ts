22aeeb54357570c5c06ad759a429731b8feb0c92
lhvision <lhvision@outlook.com>
2024-12-13 00:25:26 +0800

    feat: 改造 theme 相关函数

--- src/browser/setTheme.ts
+++ src/browser/setTheme.ts
@@ -2,7 +2,7 @@
  * 初始化主题监听器, 设置 dark 或 light 主题
  * @returns 返回一个函数，用于移除主题监听器
  */
-export function initThemeListener(setThemeCallback: (darkThemeMqMatches?: boolean) => void) {
+export function initThemeListener(setThemeCallback: (darkThemeMqMatches: boolean) => void) {
   // 检查浏览器是否支持主题监听
   if (window.matchMedia) {
     // 创建媒体查询
@@ -25,10 +25,7 @@ export function setTheme(isDark?: boolean) {
   document.documentElement.classList.toggle('dark', isDark)
 }
 
-/** 使用 view transition 设置主题 */
-export async function setThemeInViewTransition(isDark?: boolean, startViewTransitionCallback?: () => void) {
-  await document.startViewTransition(() => {
-    setTheme(isDark)
-    startViewTransitionCallback?.()
-  }).ready
+/** startViewTransition Api */
+export async function startViewTransition(startViewTransitionCallback: () => void) {
+  await document.startViewTransition(startViewTransitionCallback).ready
 }
